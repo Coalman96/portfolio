ea1b32b0c5a48b9967725fb17ca01d08540b1cef
이상민 <mayuaa001@gmail.com>
2023-04-23 17:08:22 +0900

    1차작업완료

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      //min-width
+      mobile: { max: "768px" },
+      pc: "769px",
+    },
+
+    extend: {
+      fontFamily: {
+        myFont: "Playfair Display",
+      },
+    },
+  },
+  plugins: [],
+};
